e95287d84d055767cfb1f20a770a03e8228810bf
Full Name <mohabelbasiry@sarri.sa>
2024-06-30 15:54:38 +0300

    update add product

--- app/product/add/components/productVariations/variationTables/index.jsx
+++ app/product/add/components/productVariations/variationTables/index.jsx
@@ -2,18 +2,11 @@ import { InputWithLabelComponent } from "@/components/inputcomponent";
 import { useEffect, useState } from "react";
 import { generateQualities } from "./generateQualitiesOptions";
 import { getKeysAndValues } from "./getUniqukeysAndValue";
-import { TableData } from "./tableBody/tableData";
-import { SearchInput } from "./Filters/Search";
-import { FiltersKeys_values } from "./Filters/FiltersKeys_values";
-import { filterData } from "./Filters/function/FilterFunction";
 
 export const VariationTable = ({ varitions }) => {
+  console.log(varitions, "varitions");
   const [autoGenerate, setAutoGenerate] = useState([]);
   const isAr = "ar";
-  const [filters, setFilters] = useState({
-    filterByValues: [],
-    setSearch: "",
-  });
   const updatedVaration = varitions?.map((item) => {
     return {
       key_en: item?.option,
@@ -34,25 +27,7 @@ export const VariationTable = ({ varitions }) => {
       console.log(varitions,'varitions')
       setAutoGenerate(generateQualities(varitions));
     }
-    setAutoGenerate((prev) => {
-      if (filters?.filterByValues?.length) {
-        const Filtered = filterData(
-          generateQualities(varitions),
-          filters?.filterByValues
-        );
-        return Filtered;
-      } else {
-        return generateQualities(varitions);
-      }
-
-      // return FilteredOutByValuesAndKeys;
-    });
-  }, [filters, varitions]);
-
-  // useEffect(() => {
-
-  // }, [filters, varitions]);
-
+  }, [varitions]);
   return (
     <div className="w-[100%] mt-3 bg-[#eeeeee7d] p-2 rounded-3 border shadow-md">
       <div className="flex justify-between items-center px-2">
@@ -65,34 +40,235 @@ export const VariationTable = ({ varitions }) => {
             selectArray={["hello", "dsdsa", "dasdas"]}
           />
         </div>
-        <div className="GroupBy flex items-center  text-sm gap-3">
-          <p>Sort by</p>
-          <InputWithLabelComponent
-            Input={false}
-            PlaceHolder="Group By"
-            inputCss="w-fit !p-1 shadow border border-[#ddd]  "
-            selectArray={["hello", "dsdsa", "dasdas"]}
-          />
-        </div>
       </div>
       <div className="searchSortCOmponent">
-        {/* <InputWithLabelComponent
+        <InputWithLabelComponent
           Input
           inputCss="border border-[#333] !p-1 !px-2 !text-black "
           PlaceHolder="search"
-          onChange={(e) => HandleEventSearch(e)}
-        /> */}
-        <SearchInput
-          setAutoGenerate={setAutoGenerate}
-          autoGenerate={autoGenerate}
-          filter={filters}
         />
-        <FiltersKeys_values setFilters={setFilters} varitions={varitions} />
+
+        <div className="grid grid-cols-6 gap-3">
+          {varitions?.map((item) => {
+            return (
+              <InputWithLabelComponent
+                Input={false}
+                PlaceHolder="Group By"
+                inputCss="w-fit !p-1 text-sm !px-1 shadow border border-[#ddd]  "
+                selectArray={item?.currentValues?.map((iv) => {
+                  return iv?.value_en;
+                })}
+              />
+            );
+          })}
+        </div>
       </div>
-      <TableData
-        autoGenerate={autoGenerate}
-        setAutoGenerate={setAutoGenerate}
-      />
+      <div className="w-[100%] overflow-auto">
+        {/*
+  Heads up! 👋
+
+  This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
+*/}
+
+        <div className="overflow-x-auto rounded-lg border border-gray-200">
+          <table className="min-w-full divide-y-2 divide-gray-200 bg-white text-sm ">
+            <thead className="text-left ">
+              <tr>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900  ">
+                  <input
+                    type={"checkbox"}
+                    className="!h-[25px] flex text-[25px] items-center w-[20px] p-3"
+                  />
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  Date of Birth
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  Role
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  Salary
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  Salary
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  Salary
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  Salary
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  Salary
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  Salary
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  Salary
+                </th>
+                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 sticky right-0 bg-white z-[3]">
+                  Edit
+                </th>
+              </tr>
+            </thead>
+
+            <tbody className="divide-y divide-gray-200">
+              {autoGenerate?.length ? (
+               autoGenerate?.map((item,idx)=>{
+                console.log(item,'item')
+                return( <tr>
+                  <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                    <input
+                      type={"checkbox"}
+                      className="!h-[25px] flex text-[25px] items-center w-[20px] p-3"
+                    />
+                  </td>
+                  <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                {item?.values?.map(item=>{
+                  return(<>
+                    {item?.value_en}/
+                  </>)
+                })}
+                  </td>
+             
+
+                  <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 sticky right-0   bg-white z-[3]  border-l ">
+                    <button className="p-3 border shadow h-[40px] flex items-center justify-center rounded-md text-sm">
+                      Edit
+                    </button>
+                  </td>
+                </tr>)
+               })
+              ) : null}
+
+              {/* <tr>
+                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  <input
+                    type={"checkbox"}
+                    className="!h-[25px] flex text-[25px] items-center w-[20px] p-3"
+                  />
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  24/05/1995
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  Web Developer
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+
+                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 sticky right-0   bg-white z-[3]  border-l ">
+                  <button className="p-3 border shadow h-[40px] flex items-center justify-center rounded-md text-sm">
+                    Edit
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  <input
+                    type={"checkbox"}
+                    className="!h-[25px] flex text-[25px] items-center w-[20px] p-3"
+                  />
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  24/05/1995
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  Web Developer
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+
+                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 sticky right-0   bg-white z-[3]  border-l ">
+                  <button className="p-3 border shadow h-[40px] flex items-center justify-center rounded-md text-sm">
+                    Edit
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
+                  <input
+                    type={"checkbox"}
+                    className="!h-[25px] flex text-[25px] items-center w-[20px] p-3"
+                  />
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  24/05/1995
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  Web Developer
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+                <td className="whitespace-nowrap px-4 py-2 text-gray-700">
+                  $120,000
+                </td>
+
+                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 sticky right-0   bg-white z-[3]  border-l ">
+                  <button className="p-3 border shadow h-[40px] flex items-center justify-center rounded-md text-sm">
+                    Edit
+                  </button>
+                </td>
+              </tr> */}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
