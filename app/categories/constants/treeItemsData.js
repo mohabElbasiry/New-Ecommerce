087c209847a087cf1475bff4592eb420f901db8f
Mohamed Salah <moh.salah70@gmail.com>
2024-08-01 14:55:24 +0300

    adding categories director

---
+++ app/categories/constants/treeItemsData.js
@@ -0,0 +1,50 @@
+export const treeData = [
+  {
+    id: 12314123,
+    title: "category1",
+    hasNext: false,
+    hasPrev: false,
+    // children: [
+    //   {
+    //     id: 465416,
+    //     title: "sub 1",
+    //   },
+    //   {
+    //     id: 86761253,
+
+    //     title: "sub 2",
+    //     // children: [
+    //     //   {
+    //     //     id: 7638712,
+    //     //     title: "sub sub 1",
+    //     //   },
+    //     //   {
+    //     //     id: 124645365,
+
+    //     //     title: "sub sub 2",
+    //     //   },
+    //     //   {
+    //     //     id: 7672846,
+    //     //     title: "sub sub 3",
+    //     //   },
+    //     // ],
+    //   },
+    // ],
+  },
+  {
+    id: 69657848,
+    title: "category2",
+    hasNext: false,
+    hasPrev: false,
+    // children: [
+    //   {
+    //     id: 345728456,
+    //     title: "sub sub 2",
+    //   },
+    //   {
+    //     id: 23725643276,
+    //     title: "sub sub 3",
+    //   },
+    // ],
+  },
+];
---
+++ app/categories/page.js
@@ -0,0 +1,11 @@
+import Tree from "./components/tree/Index";
+
+const page = () => {
+  return (
+    <div>
+      <Tree />
+    </div>
+  );
+};
+
+export default page;
